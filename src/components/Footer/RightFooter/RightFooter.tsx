9ed649319c37684326e7e7c2bc0dd753978a9d02
HAPKOLLIE4KA <vlad.kriv.2004@mail.ru>
2023-09-10 01:26:37 +0700

    правый футор через грид, оцентрирован при 600px

--- src/components/Footer/RightFooter/RightFooter.tsx
+++ src/components/Footer/RightFooter/RightFooter.tsx
@@ -9,37 +9,35 @@ import { useWidth } from "../../../hooks/useWidth";
 const RightFooter: FC = () => {
   return (
     <div className={styles.rightFooter}>
-      <div className={styles.rightData}>
-        <div className={styles.upRight}>
-          <div className={styles.contacts}>
-            <div className={styles.phone}>
-              <div className="icon">
-                <img src={callImg} />
-              </div>
-              <a className={styles.txt}>8 800 600-61-91</a>
+      <div className={styles.upRight}>
+        <div className={styles.contacts}>
+          <div className={styles.phone}>
+            <div className="icon">
+              <img src={callImg} />
             </div>
-            <div className={styles.iconContacts}>
-              <a className={styles.linkIcon} href="#">
-                <img src={telegramImg} />
-              </a>
-              <a className={styles.linkIcon} href="#">
-                <img src={viberImg} />
-              </a>
-              <a className={styles.linkIcon} href="#">
-                <img src={whatsappImg} />
-              </a>
-            </div>
+            <a className={styles.txt}>8 800 600-61-91</a>
           </div>
-          <span className={styles.time}>
-            Время работы: с 9:00 до 20:00 по мск
-          </span>
-        </div>
-        <div className={styles.mail}>
-          <div className="icon">
-            <img src={mailImg} />
+          <div className={styles.iconContacts}>
+            <a className={styles.linkIcon} href="#">
+              <img src={telegramImg} />
+            </a>
+            <a className={styles.linkIcon} href="#">
+              <img src={viberImg} />
+            </a>
+            <a className={styles.linkIcon} href="#">
+              <img src={whatsappImg} />
+            </a>
           </div>
-          <a className={styles.txt}>Mr.Driskell@mail.ru</a>
         </div>
+        <span className={styles.time}>
+          Время работы: с 9:00 до 20:00 по мск
+        </span>
+      </div>
+      <div className={styles.mail}>
+        <div className="icon">
+          <img src={mailImg} />
+        </div>
+        <a className={styles.txt}>Mr.Driskell@mail.ru</a>
       </div>
     </div>
   );
